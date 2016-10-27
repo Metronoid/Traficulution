8168ff553678d337053e666ac9c70ba2dc0d6240
snijderd@gmail.com <snijderd@gmail.com>
2016-10-27 12:49:55 +0200

    Commented out car shapes due to lag of not reusing them.

--- static/javascript/objects.js
+++ static/javascript/objects.js
@@ -46,32 +46,32 @@ class Car {
 
         //TODO: Code for adding actual car shapes to the scene, current issue is that memory rises fast. Commented out for now
         //
-        let self = this;
-        let ploader = new THREE.ColladaLoader();
-        ploader.load('/model/car.dae', function (result) {
-            self.spawn = spawns[spawnPoint];
-            self.mesh = result.scene;
-            self.mesh.position.x = self.spawn.position.x;
-            self.mesh.position.y = self.spawn.position.y;
-            self.mesh.position.z = self.spawn.position.z;
-            self.mesh.quaternion.setFromAxisAngle(new THREE.Vector3(0,0,0), 0);
-            self.mesh.rotation.y = self.spawn.rotation;
+        // let self = this;
+        // let ploader = new THREE.ColladaLoader();
+        // ploader.load('/model/car.dae', function (result) {
+        //     self.spawn = spawns[spawnPoint];
+        //     self.mesh = result.scene;
+        //     self.mesh.position.x = self.spawn.position.x;
+        //     self.mesh.position.y = self.spawn.position.y;
+        //     self.mesh.position.z = self.spawn.position.z;
+        //     self.mesh.quaternion.setFromAxisAngle(new THREE.Vector3(0,0,0), 0);
+        //     self.mesh.rotation.y = self.spawn.rotation;
+        //
+        //     self.raycaster = new THREE.Raycaster();
+        //     self.raycaster.set(result.scene.position, new THREE.Vector3(0, -1, 0))
+        //
+        //     scene.add(result.scene);
+        // });
 
-            self.raycaster = new THREE.Raycaster();
-            self.raycaster.set(result.scene.position, new THREE.Vector3(0, -1, 0))
-
-            scene.add(result.scene);
-        });
-
-        // this.spawn = spawns[spawnPoint];
-        // this.mesh.position.x = this.spawn.position.x;
-        // this.mesh.position.y = this.spawn.position.y;
-        // this.mesh.position.z = this.spawn.position.z;
-        // this.mesh.quaternion.setFromAxisAngle(new THREE.Vector3(0,0,0), 0);
-        // this.mesh.rotation.y = this.spawn.rotation;
-        // scene.add(this.mesh);
-        // this.raycaster = new THREE.Raycaster();
-        // this.raycaster.set(this.mesh.position, new THREE.Vector3(0, -1, 0))
+        this.spawn = spawns[spawnPoint];
+        this.mesh.position.x = this.spawn.position.x;
+        this.mesh.position.y = this.spawn.position.y;
+        this.mesh.position.z = this.spawn.position.z;
+        this.mesh.quaternion.setFromAxisAngle(new THREE.Vector3(0,0,0), 0);
+        this.mesh.rotation.y = this.spawn.rotation;
+        scene.add(this.mesh);
+        this.raycaster = new THREE.Raycaster();
+        this.raycaster.set(this.mesh.position, new THREE.Vector3(0, -1, 0))
 
     }
 }
