4fb72fd2999d00f13d924cbbed623eb9edd43f14
Wander van der Wal <wvdwal@hotmail.nl>
2016-11-02 21:53:48 +0100

    Added a random mutation in the beginning.

--- static/javascript/mutations.js
+++ static/javascript/mutations.js
@@ -9,16 +9,16 @@ var randomMutate = function (weight) {
 }
 
 var superMutate = function (weight) {
-    //let newWeight = Math.random() - 0.5;
-    //newWeight = Math.min(newWeight,0.5);
-    //newWeight = Math.max(newWeight,-0.5);
-    return weight
+    let newWeight = Math.random() - 0.5;
+    newWeight = Math.min(newWeight,0.5);
+    newWeight = Math.max(newWeight,-0.5);
+    return newWeight
     //return weight
 }
 
 var slideMutate = function (weight,max,min,mutateChance) {
     if (Math.random() >= mutateChance) {
-        let newWeight = weight + Math.random() * (max*2) - max;
+        let newWeight = weight + (Math.random() * (max*2) - max);
         newWeight = Math.min(newWeight,max);
         newWeight = Math.max(newWeight,min);
         return newWeight;
